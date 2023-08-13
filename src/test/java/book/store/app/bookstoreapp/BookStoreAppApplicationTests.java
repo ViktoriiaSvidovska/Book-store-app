5a127944d8e710d74658b0f5812000cb1b4042dc
Viktoriia <reraitviktoria@gmail.com>
2023-08-13 13:10:22 +0200

    first commit

---
+++ src/test/java/book/store/app/bookstoreapp/BookStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package book.store.app.bookstoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
