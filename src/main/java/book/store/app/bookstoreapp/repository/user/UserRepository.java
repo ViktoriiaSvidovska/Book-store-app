8fd8da30654e4fe67302ecf2c18624ec9047cba5
Viktoriia <reraitviktoria@gmail.com>
2023-09-13 16:34:38 +0200

    Tested and add new changes

--- src/main/java/book/store/app/bookstoreapp/repository/user/UserRepository.java
+++ src/main/java/book/store/app/bookstoreapp/repository/user/UserRepository.java
@@ -2,7 +2,6 @@ package book.store.app.bookstoreapp.repository.user;
 
 import book.store.app.bookstoreapp.model.User;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
